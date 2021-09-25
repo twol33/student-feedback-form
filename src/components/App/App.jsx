f33b1c9187a72383438925514b6ca824cb71ef00
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-24 22:49:12 -0500

    added router and created component for each path, imported history and router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function App() {
+  const history = useHistory();
+
+  function submitFeeling() {
+    history.push('/')
+  }
 
   return (
     <div className='App'>
@@ -10,6 +17,19 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <h1>How are you feeling today?</h1>
+
+      <h3>Feeling?</h3>
+      <input type = 'number' min = '1' max = '5'/>
+      
+
+      <Router>
+        <button onClick={submitFeeling}>NEXT</button>
+        <Route exact path='/'></Route>
+        <Route exact path='/2'></Route>
+        <Route exact path='/3'></Route>
+        <Route exact path='/4'></Route>
+      </Router>
     </div>
   );
 }
